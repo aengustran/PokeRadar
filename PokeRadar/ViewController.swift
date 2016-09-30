565c05881c7e73f0b25bf68fd4e507f5bf223a3a
Aengus Tran <aengustran@gmail.com>
2016-09-30 20:46:52 +1000

    2nd Commit: Finish UIMapview and UIButton

--- PokeRadar/ViewController.swift
+++ PokeRadar/ViewController.swift
@@ -7,18 +7,102 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    let locationManager = CLLocationManager()
+
+    lazy var mapView: MKMapView = {
+        let mv = MKMapView()
+        mv.delegate = self
+        mv.translatesAutoresizingMaskIntoConstraints = false
+        mv.userTrackingMode = MKUserTrackingMode.followWithHeading
+        return mv
+    }()
+
+    func SetupMapView() {
+        mapView.frame = view.frame
+        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        mapView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalToConstant: view.frame.height).isActive = true
+        mapView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        requestAuthorization()
+
+    }
+
+
+    lazy var pokemonButton: UIButton = {
+        let bnt = UIButton()
+        bnt.translatesAutoresizingMaskIntoConstraints = false
+        bnt.addTarget(self, action: #selector(handlepokemonButton), for: .touchUpInside)
+        return bnt
+    }()
+
+
+    func handlepokemonButton() {
+        print("Handle pokemon button")
+    }
+
+
+
+    func SetupPokemonButton() {
+
+        let image = UIImage(named: "pokeball")
+        pokemonButton.setImage(image, for: .normal)
+        pokemonButton.contentMode = .scaleAspectFit
+        pokemonButton.leftAnchor.constraint(equalTo: mapView.leftAnchor, constant: 20).isActive = true
+        pokemonButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -20).isActive = true
+        pokemonButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        pokemonButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+
+
+
+
+    func requestAuthorization() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+
+    }
+
+
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        mapView.showsUserLocation = true
+    }
+
+
+
+    var locationSetOnce = false
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+
+        if !locationSetOnce {
+            let currentRegion = MKCoordinateRegionMakeWithDistance((userLocation.location?.coordinate)!, 2000, 2000)
+            mapView.setRegion(currentRegion, animated: true)
+            locationSetOnce = true
+        }
+
+    }
+
+
+    override func viewDidAppear(_ animated: Bool) {
+        requestAuthorization()
+    }
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(mapView)
+        SetupMapView()
+        view.addSubview(pokemonButton)
+        SetupPokemonButton()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
